2abfa44f99fba8fd340b8ddc5d23092b00088fd5
name <email>
2026-05-04 02:57:57 -0400

    varroa retification test

--- beevision/src/beevision/pipeline/analyze_frame.py
+++ beevision/src/beevision/pipeline/analyze_frame.py
@@ -44,10 +44,12 @@ if __name__ == "__main__":
     path5 = "/Users/ethanlin/CSCI1430_Homeworks/Comb-puter-Vision/beevision/data/test_images_rectification/tilted_beehive_5.jpg"
     path6 = "/Users/ethanlin/CSCI1430_Homeworks/Comb-puter-Vision/beevision/data/test_images_rectification/tilted_beehive_6.jpg"
     path8 = "/Users/ethanlin/CSCI1430_Homeworks/Comb-puter-Vision/beevision/data/test_images_rectification/tilted_beehive_8.jpg"
+    path_varroa = "/Users/ethanlin/CSCI1430_Homeworks/Comb-puter-Vision/beevision/data/test_images_rectification/tilted_beehive_with_varroa.jpg"
     # analyze_frame(path, 1) 
     analyze_frame(path7, 7) 
     analyze_frame(path3, 3) 
     analyze_frame(path5, 5) 
     analyze_frame(path6, 6) 
     analyze_frame(path8, 8) 
+    analyze_frame(path_varroa, 9) 
     # analyze_frame(path3, 3) 
